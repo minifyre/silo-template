32ed122f07cb3f1ce39bd0b8cb2b814f966adce6
Minifyre <minifyre@gmail.com>
2018-10-09 12:40:23 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,5 +1,19 @@
 import silo from './output.mjs'
-import truth from './node_modules/truth/truth.mjs'
-const {config,input,logic,output,util}=silo
-
-export default silo
+const
+{config,input,logic,output,util}=silo,
+{truth,v}=util
+export default async function custom(url='/node_modules/custom-element/')
+{
+	await silo(url,'custom-element',custom.viewer)
+}
+Object.assign(pixel,silo)
+custom.viewer=class extends silo.viewer
+{
+	constructor(state={})
+	{
+		super(state)
+		let renderer=x=>x
+		this.state=truth(logic(state),(...args)=>renderer(args))
+		renderer=v.render(this.shadowRoot,this,output)
+	}
+}
