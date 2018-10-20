290780370cb15c61a203f9bb48355aeea72fdc40
Minifyre <minifyre@gmail.com>
2018-10-20 13:06:29 -0500

    +util.mkCustomEl

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ const
 //@todo find & replace "custom" & "element" with proper values
 export default async function custom(url='/node_modules/custom-element/')
 {
-	await silo(url,'custom-element',custom.element)
+	await util.mkCustomEl(url,'custom-element',custom.element)
 }
 Object.assign(custom,silo)
 custom.element=class extends silo.viewer
