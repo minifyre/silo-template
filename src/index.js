b835b2c374174e0d53b3cc7de2593d1e3b3b359e
Minifyre <minifyre@gmail.com>
2018-12-09 17:50:47 -0600

    used new silo/index.js file for base

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import silo from './output.mjs'
-const
-{config,input,logic,output,util}=silo,
-{truth,v}=util
+import silo from './node_modules/silo/index.js'
+import truth from './node_modules/truth/truth.mjs'
+import v from './node_modules/v/v.mjs'
+
+const {config,util,logic,output,input}=silo
 //@todo find & replace "custom" & "element" with proper values
-export default async function custom(url='/node_modules/custom-element/')
+export default silo(async function(initialState,url='/node_modules/custom-element/')
 {
+	const
+	state=logic(initialState),
+	render=truth.compile(({state})=>v.render(document.body,state,output))
+
 	await util.mkCustomEl(url,'custom-element',custom.element)
-}
-Object.assign(custom,silo)
-custom.element=class extends silo.viewer
+
+
+	truth(state,render)
+})
+
+silo.element=class extends silo.viewer
 {
 	constructor(state={})
 	{
